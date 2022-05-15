54b8033bb5bc7bb01ca88ae5dbffab2cadd22047
fecapark <fecapark@gmail.com>
2022-05-15 10:55:41 +0000

    Change core's test files

--- src/core/Router/routes.test.ts
+++ src/core/Router/routes.test.ts
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { ROUTES } from "./routes";
 
 describe("Test routes module.", () => {
@@ -11,5 +15,39 @@ describe("Test routes module.", () => {
     expect(() => {
       ROUTES.INFO[testPath].view();
     }).toThrow("test1");
+  });
+});
+
+describe("Test routes module: split proxy path", () => {
+  it("Remove a proxy path.", () => {
+    const { proxy, path }: { proxy: string; path: string } =
+      ROUTES.splitProxyPath("/proxy/23/");
+
+    expect(proxy).toBe("/proxy/23");
+    expect(path).toBe("/");
+  });
+
+  it("Remove nested proxy paths.", () => {
+    const { proxy, path }: { proxy: string; path: string } =
+      ROUTES.splitProxyPath("/proxy/23/proxy/42/");
+
+    expect(proxy).toBe("/proxy/23/proxy/42");
+    expect(path).toBe("/");
+  });
+
+  it("Remain real routes.", () => {
+    const { proxy, path }: { proxy: string; path: string } =
+      ROUTES.splitProxyPath("/proxy/23/hello");
+
+    expect(proxy).toBe("/proxy/23");
+    expect(path).toBe("/hello");
+  });
+
+  it("Remain routes contain proxy path.", () => {
+    const { proxy, path }: { proxy: string; path: string } =
+      ROUTES.splitProxyPath("/proxy/23/hello/proxy/3000/");
+
+    expect(proxy).toBe("/proxy/23");
+    expect(path).toBe("/hello/proxy/3000/");
   });
 });
